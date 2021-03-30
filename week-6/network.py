2a4f5bcd705dcb2ce9362d379451340b374809f4
Yohenba18 <roddik12kshetri@gmail.com>
2021-03-30 14:53:56 +0530

    week6 q1

--- week-6/network.py
+++
@@ -1,9 +0,0 @@
-import socket
-import sys
-ser = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
-host =  'Local Host'
-port = 7070
-ser.bind()
-ser_add = ((host,port))
-print('Trying to connect')
-ser.connect(ser_add)
